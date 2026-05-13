2bb3c14aba3af8356ddd5ed76c9a3d24336ce646
Keyvan Mosharraf <keyvan@keyvanm.com>
2026-05-13 17:24:20 -0400

    New refactored editors

--- src/zyn/editors.py
+++ src/zyn/editors.py
@@ -11,48 +11,67 @@ SOCKETS_DIR = Path(os.environ.get("XDG_RUNTIME_DIR", "/tmp")) / "zyn"
 
 @dataclass
 class Editor:
-    root: Path
-    socket: Path | None = field(default=None)
+    """An interface to a Zyn compatible editor"""
+
+    root: Path | None = field(default=None)
+    """The workspace root, a dir, could be empty to indicate not being tied to a workspace"""
+    session_socket: Path | None = field(default=None)
+    """If this instance is assosiated with a session, here's the socket"""
 
     @staticmethod
-    def socket_path_for(root: Path) -> Path:
-        key = hashlib.md5(str(root.resolve()).encode()).hexdigest()
+    def get_socket_for(path: Path) -> Path:
+        key = hashlib.md5(str(path.resolve()).encode()).hexdigest()
         return SOCKETS_DIR / f"{key}.sock"
 
     @classmethod
-    def discover(cls, path: Path) -> Self | None:
+    def _discover_active_workspace_session(cls, path: Path) -> tuple[Path, Path | None]:
         dir_path = path if path.is_dir() else path.parent
         for directory in itertools.chain([dir_path], dir_path.parents):
-            sock = cls.socket_path_for(directory)
-            if sock.is_socket():
-                return cls(root=directory.resolve(), socket=sock)
-        return None
+            sock = cls.get_socket_for(directory)
+            if sock.is_socket():  # TODO: We should also check if the socket isn't stale
+                return directory, sock
+        return dir_path, None
 
-    def ensure_socket(self) -> Path:
-        SOCKETS_DIR.mkdir(parents=True, exist_ok=True)
-        if self.socket is None:
-            self.socket = self.socket_path_for(self.root)
-        return self.socket
+    @classmethod
+    def discover(cls, path: Path) -> Self | None:
+        directory, socket = cls._discover_active_workspace_session(path)
+        return cls(root=directory.resolve(), session_socket=socket)
+
+    def convert_to_session(self):
+        if not self.root:
+            raise ValueError("Needs to be rooted")
+
+        self.session_socket = self.get_socket_for(self.root)
 
     def __enter__(self) -> Self:
         return self
 
     def __exit__(self, *_) -> None:
-        if self.socket:
-            self.socket.unlink(missing_ok=True)
+        if self.session_socket:
+            self.session_socket.unlink(missing_ok=True)
 
-    def open(self, file: Path) -> None:
+    def launch(self):
         raise NotImplementedError()
 
-    def start(self, file: Path) -> None:
-        raise NotImplementedError()
+    def open(self, path: Path):
+        raise NotImplementedError
 
 
 class Neovim(Editor):
-    def open(self, file: Path) -> None:
-        assert self.socket
-        subprocess.run(["nvim", "--server", str(self.socket), "--remote", str(file)])
+    def launch(self):
+        args = ["nvim"]
 
-    def start(self, file: Path) -> None:
-        sock = self.ensure_socket()
-        subprocess.run(["nvim", "--listen", str(sock), str(file)])
+        if self.session_socket:
+            args.extend(("--server", str(self.session_socket)))
+
+        if self.root:
+            args.extend(("--remote", str(self.root)))
+
+        subprocess.run(args)
+
+    def open(self, path: Path):
+        args = ["nvim"]
+        if self.session_socket:
+            if self.session_socket:
+                args.extend(("--server", str(self.session_socket)))
+        subprocess.run([*args, str(path)])
